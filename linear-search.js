66c437fab2592d977c49d59a8711776731c91b25
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-13 19:39:17 +0300

    OK

--- linear-search.js
+++ linear-search.js
@@ -2,13 +2,26 @@
 //принимает два аргумента: объект и массив. Она должна последовательно перебирать каждый элемент и возвращать индекс,
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
-
+  for(let i = 0; i < arr.length; i++){
+      if(arr[i] === n){
+          return i
+      }
+  }
+  return -1
 }
-
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
-
+  let res = []
+  for(let i = 0; i< arr.length; i++){
+    if(arr[i] === n){
+      res.push(i);
+    }
+  }
+  if(!res){
+    return -1;
+  }
+  return res;
 }
 
 module.exports = {
